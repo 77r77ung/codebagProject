c41e089e0b9cecbb54711d6b7c383a14874d3373
77r77ung <2625k@naver.com>
2021-12-08 21:51:32 +0900

    Revert "Codebag Project First Commit"
    This reverts commit da6c3f9b1cd5ff8bcc203ed09addb13a04aa3bb1.

--- src/query/usersQuery.js
+++
@@ -1,7 +0,0 @@
-/* signIn(로그인), signUp(회원가입) */
-
-// signUp
-exports.signUp = 'insert into users (users_uid, users_password, users_nickname, users_tel, users_email, users_job, users_genre) values(?, ?, ?, ?, ?, ?, ?)'
-
-// signIn
-exports.signIn = 'select * from users where users_uid=? and users_password=?'
--- src/routes/mainRoute.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const questionController = require('../controllers/questionController')
-
-// 책 정보
-router.get('/', questionController.listQuestion);
-
-module.exports = router;
