a648231d6500d0df5fcd9920748bc7c5f129d0ef
77r77ung <2625k@naver.com>
2021-12-08 22:04:13 +0900

    Codebag Project
    !! 서버 돌아가는데 오류 발생 시: app.js / package.json 확인해주세요 (주석 확인)
    ** Todo List (현재 UI 기준 Front만!)
    1. 메인 화면 한 개의 행에 2개 card 위치하도록
    2. 메인 화면이 아닌 다른 화면 들어가면 logo 이미지 사라짐
    3. 로그인 / 회원가입 화면에서는 최신순 / 인기순이 만들어둔 UI navbar처럼 변하게 수정 / 검색어 툴바 사라지게!
    4. 메뉴바, 최신순, 인기순: 최신순 밖에 안 눌러지게 해둠 메뉴(사이드)바, 인기순도 선택되게 수정

---
+++ database/2021128_dump/codebag_answer.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.25, for macos11 (x86_64)
+--
+-- Host: 127.0.0.1    Database: codebag
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `answer`
+--
+
+DROP TABLE IF EXISTS `answer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `answer` (
+  `answer_uid` int NOT NULL AUTO_INCREMENT,
+  `answer_content` varchar(1000) DEFAULT NULL,
+  `answer_date` datetime DEFAULT NULL,
+  `like_count` int DEFAULT NULL,
+  `question_question_uid` int NOT NULL,
+  `users_users_uid` varchar(20) NOT NULL,
+  PRIMARY KEY (`answer_uid`),
+  KEY `fk_answer_question_idx` (`question_question_uid`),
+  KEY `fk_answer_users1_idx` (`users_users_uid`),
+  CONSTRAINT `fk_answer_question` FOREIGN KEY (`question_question_uid`) REFERENCES `question` (`question_uid`),
+  CONSTRAINT `fk_answer_users1` FOREIGN KEY (`users_users_uid`) REFERENCES `users` (`users_uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `answer`
+--
+
+LOCK TABLES `answer` WRITE;
+/*!40000 ALTER TABLE `answer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `answer` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-08 22:03:18
---
+++ database/2021128_dump/codebag_question.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.25, for macos11 (x86_64)
+--
+-- Host: 127.0.0.1    Database: codebag
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `question` (
+  `question_uid` int NOT NULL AUTO_INCREMENT,
+  `question_title` varchar(500) DEFAULT NULL,
+  `question_content` varchar(1000) DEFAULT NULL,
+  `question_date` datetime DEFAULT NULL,
+  `views` int DEFAULT NULL,
+  `like_count` int DEFAULT NULL,
+  `users_users_uid` varchar(20) NOT NULL,
+  PRIMARY KEY (`question_uid`),
+  KEY `fk_question_users1_idx` (`users_users_uid`),
+  CONSTRAINT `fk_question_users1` FOREIGN KEY (`users_users_uid`) REFERENCES `users` (`users_uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question`
+--
+
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (1,'제목입니다.','내용입니다.','2021-12-08 00:00:00',0,0,'lsy'),(2,'질문 있습니다.','사실 없습니다.','2021-12-08 00:00:00',0,0,'lsy');
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-08 22:03:18
