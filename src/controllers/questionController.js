4397f43c919ab4d488f444493657e85765548aff
yemson <lier@kakao.com>
2021-12-13 12:36:39 +0900

    출동 수정

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -1,12 +1,37 @@
 var questionService = require('../services/questionService')
+var answerService = require('../services/answerService')
 const pool = require('../../database/codebag_db')
 
-exports.addQuestion = async(req, res) => {
-    const {question_title, question_content, question_date, views, like_count, users_users_uid} = req.body;
-    const {question_uid} = req.params;
+exports.detail = async (req, res) => {
+    const { question_uid } = req.params
 
-    let session = req.session.users_uid;
     try{
+        let question_info = await questionService.detailQuestion(question_uid)
+        // let answer_info = await answerService.listAnswer(question_qeustion_uid)
+        let session = req.session.users_nickname
+
+        return res.render('main', {
+            page: './question/detailquestion',
+            session: session,
+            question_info: question_info,
+            // answer_info: answer_info
+        })
+    }catch(err){
+        return res.status(500).json(err)
+    }
+}
+
+exports.addQuestion = async(req, res) => {
+    const { question_title, question_content } = req.body;
+    const { session } = req.params;
+
+    try{
+        let question_date = new Date()
+        let views = 0
+        let like_count = 0
+        let users_users_uid = req.session.users_uid;
+        
+
         if(session != Null){
             await questionService.addQuestion(question_uid, question_title, question_content, question_date, views, like_count, users_users_uid);
         }else{
@@ -21,7 +46,19 @@ exports.addQuestion = async(req, res) => {
     }
 }
 
-exports.updateQuestion = async(req, res) => {
+exports.addQuestionPage = async(req, res) => {
+    try{
+        let session = req.session.users_uids
+        return res.render('main', {
+            page:'./question/addquestion',
+            session:session
+        })
+    }catch(err){
+        return res.status(500).json(err)
+    }
+}
+
+exports.updateQuestion = async (req, res) => {
     let {question_title, question_content, question_date} = req.body;
     let {question_uid} = req.params;
     try{
@@ -32,11 +69,26 @@ exports.updateQuestion = async(req, res) => {
     }
 }
 
+exports.updateQuestionPage = async (req, res) => {
+    const { question_uid } = req.params
+    try{
+        let update_info = await questionService.detailQuestion(question_uid)
+        let session = req.session.users_uid
+        return res.render('mian', {
+            page:'./question/updateQuestion',
+            session:session,
+            update_info:update_info
+        })
+    }catch(err){
+        return res.status(500).json(err)
+    }
+}
+
 exports.deleteQuestion = async (req, res) => {
-    let {question_uid} = req.params
+    let { question_uid } = req.params
     try{
         await questionService.deleteQuestion(question_uid)
-        return res.redirect('/')
+        return res.redirect('/' + req.session.users_uid)
     }catch(err){
         return res.status(500).json(err);
     }
@@ -45,28 +97,12 @@ exports.deleteQuestion = async (req, res) => {
 exports.listQuestion = async (req, res) => {
     try{
         let question_info = await questionService.listQuestion();
-        let users_nickname = req.session.users_nickname;
+        let session = req.session.users_nickname;
+        console.log(question_info);
         return res.render('main', {
             page: './question/listquestion',
-            session: req.session.users_uid,
-            question_info: question_info,
-            users_nickname : users_nickname
-        })
-    }catch(err){
-        return res.status(500).json(err);
-    }
-}
-
-exports.detailQuestion = async (req, res) => {
-    let {question_uid} = req.params
-
-    try{
-        let detail_info = await questionService.detailQuestion(question_uid)
-        let session = req.session.users_uid
-        return res.render('main', {
-            page:'./question/detailquestion',
             session: session,
-            detail_info: detail_info
+            question_info: question_info
         })
     }catch(err){
         return res.status(500).json(err);
